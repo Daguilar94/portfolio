cf1a68e770a7fd0b7edcc09c1d0331ad3b15c2ef
David Aguilar <davidaguilarh.94@gmail.com>
2017-12-05 22:50:27 -0500

    Add smooth scrolling feature

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,7 @@
 $(document).ready(function(e){
-  $('.project-trigger').click(function() {
+  $('.project-trigger, .nav-bar a').click(function() {
     $('.project-trigger').removeClass('active')
+    $('.nav-bar a').removeClass('active')
     $(this).addClass('active')
     $('.modal').fadeOut(200)
     const data = $(this).data('pro')
@@ -26,4 +27,31 @@ $(document).ready(function(e){
   },function() {
     $(this).children('i').css('color', '#fff')
   })
+
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('.parallax').animate({
+        scrollTop: $(hash).offset().top
+      }, 400, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+
+  $(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+    console.log(scroll);
+  });
+
 })
